b1808b4587226e7cd225345ce41d4f41eedbd9e4
Gabriel Rocha <gabriel.lbr@outlook.com>
2021-06-27 22:28:39 -0300

    WIP - Page Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    //<Home />
-    <NewRoom />
+  return (    
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useHistory } from 'react-router-dom';
+
+import { auth, firebase } from '../services/firebase';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg         from '../assets/images/logo.svg';
 import googleIconImg   from '../assets/images/google-icon.svg';
@@ -11,6 +15,20 @@ import { Button } from '../components/Button';
 //webpack (snowpack, vite, ...)
 
 export function Home() {
+
+  const history = useHistory();
+
+  function handleCreateRoom() {
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+    });
+    
+    //history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -21,7 +39,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask Logo" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com Google
           </button>
